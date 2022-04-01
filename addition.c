8a73b153bdbfb4e4b5d697e6bfa7cb997459580b
LokeshChahare <101494192+LokeshChahare@users.noreply.github.com>
2022-04-01 12:34:05 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void clrscr();
+void main()
+{
+    void clrscr();
+    char a[3];
+    printf("Enter the character: ");
+    scanf("%s" ,&a);
+    printf("The letter is: %s",a);
+    
+    getch();
+    
+
+}
+   
+    
+
+
+
+
+
